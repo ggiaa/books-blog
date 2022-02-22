394460da8ae646f7b94f35453fd6f40c9aa5c1c0
Gia <giaa601@gmail.com>
2022-02-22 13:06:21 +0700

    memperbaiki routes untuk halaman about dan home

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,9 +9,23 @@ class BookController extends Controller
 {
     public function index()
     {
+        return view('home', [
+            "title" => "HOME"
+        ]);
+    }
+
+    public function allbooks()
+    {
         return view('books', [
             "title" => "BOOKS",
             "books" => Book::all()
+        ]);
+    }
+
+    public function about()
+    {
+        return view('about', [
+            "title" => "ABOUT"
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home', [
-        "title" => "HOME"
-    ]);
-});
-
-Route::get('/books', [BookController::class, 'index']);
-
-Route::get('/about', function () {
-    return view('about', [
-        "title" => "ABOUT"
-    ]);
-});
+Route::get('/', [BookController::class, 'index']);
+Route::get('/books', [BookController::class, 'allbooks']);
+Route::get('/about', [BookController::class, 'about']);
